db8c0c79da3c350b71731145301b103447507628
KSGreene <210071946.aston.ac.uk>
2025-02-08 17:20:23 +0000

    change

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ def get_security_groups():
 	client = boto3.client("ec2")
 	response = client.describe_security_groups()
 	results = []
-    
     for securityGroups in response['SecurityGroups']:
         groupName = securityGroups['GroupName']
         groupId = securityGroups['GroupId']
-        
+
         for permissions in securityGroups.get('IpPermissions', []):
             if 'IpRanges' in permissions:
                 for ipRange in permissions['IpRanges']:
@@ -18,7 +17,7 @@ def get_security_groups():
                             "Security Group": groupName,
                             "Group ID": groupId,
                             "Open Port": permissions.get('FromPort', 'All'),
-                            "Risk": "Public access detected!"
+                            "Risk": "Public access detected, open ports"
                         })
 
     return results
@@ -26,6 +25,12 @@ def get_security_groups():
 
 	#jresp = json.dumps(response, indent=4)
 	#print(jresp)
+
+def save_to_json_file(data):
+	with open('securityResults.json', 'w') as f:
+		json.dump(data, f, indet=4)
+	print("Security scan results saved to securityResults.json")
+	
 
 if __name__ == '__main__':
     get_security_groups() 
