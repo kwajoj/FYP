859326db6618bd8441e1966689872c998c564428
KSGreene <210071946.aston.ac.uk>
2025-02-08 14:51:08 +0000

    first change

---
+++ main.py
@@ -0,0 +1,10 @@
+import boto3
+import json
+
+def get_security_groups:
+	response = client.describe_security_groups()
+	jresp = json.dumps(response)
+	print(jresp)
+
+if __name__ == '__main__':
+    get_security_groups() 
