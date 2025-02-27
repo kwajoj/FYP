9bdbb7d88a3a95588493990442233326688c786b
kwajoj <kwajojj16@gmail.com>
2025-02-27 18:20:29 +0000

    another change

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 import argparse
-from SG import sg_main
-from IAM import IAM_main
-from s3buckets import s3_main
+from Scanner.SG import sg_main
+from Scanner.IAM import IAM_main
+from Scanner.s3buckets import s3_main
 
 
 def main():
@@ -10,6 +10,7 @@ def main():
 	parser.add_argument("-sg", help="Check Security Groups",action="store_true")
 	parser.add_argument("-iam", help="Check IAM Policies", action="store_true")
 	parser.add_argument("-s3", help="Check S3 Buckets", action="store_true")
+	parser.add_argument("-all", help="Check for all (s3, IAM, Security Groups)", action="store_true")
 
 	args = parser.parse_args()
 
@@ -27,6 +28,18 @@ def main():
 		print("Scanning S3 Buckets...")
 		results = s3_main()
 		print(results)
+
+	if args.all:
+		print("Scanning Security Groups...")
+		resultsSG = sg_main()
+		print(resultsSG)
+		print("Scanning IAM policies...")
+		resultsIAM = IAM_main()
+		print(resultsIAM)
+		print("Scanning S3 Buckets...")
+		resultsS3 = s3_main()
+		print(resultsS3)
+
 
 
 if __name__ == '__main__':
